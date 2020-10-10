6f3192160951da48c4e68349bfc0420974e7b350
Sufiandy Elmy <elmysufiandy09@gmail.com>
2020-10-10 18:39:43 +0700

    Initial Commit

---
+++ Prediction Handwriting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prediction Handwriting
+//
+//  Created by Sufiandy Elmy on 10/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
